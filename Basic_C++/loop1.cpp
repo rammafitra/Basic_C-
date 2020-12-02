4044f949d18b173833b10afe69b2190b7a91a689
rammafitra <73507795+rammafitra@users.noreply.github.com>
2020-12-02 15:08:51 +0700

    Add files via upload

---
+++ Basic_C++/loop1.cpp
@@ -0,0 +1,16 @@
+//~ write a program in C++ to find the first 10 natural numbers
+#include <iostream>
+using namespace std;
+void natural_number()
+{
+	for(int i=0; i<10; i++)
+	{
+		std::cout<<i<<std::endl;
+	}	
+}
+
+
+int main()
+{
+	natural_number();	
+}
