58a1d5a9a905282a2ff9b5e88927a7c2a9324f3b
rammafitra <73507795+rammafitra@users.noreply.github.com>
2020-12-03 10:38:35 +0700

    class definition

---
+++ Basic_C++/oop1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Circle				//class Name
+{
+	private:
+		float radius;		//data members (variables)
+		string color;
+	public:
+		float getRadius;	// Member Functions 
+		float getArea;
+};
+
+int main()
+{
+	
+	
+}
